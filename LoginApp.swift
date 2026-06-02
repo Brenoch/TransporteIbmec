f2fb9e1144600114967275f0b9957f39148f351c
Breno <brenochavesdsp@gmail.com>
2026-06-02 10:49:13 -0300

    teste

---
+++ LoginApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            // Temporariamente inicia a view de cadastro para facilitar testes
+            CadastroAlunoView()
+        }
+    }
+}
